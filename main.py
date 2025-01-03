b158244e0df679c877e179a29f965c39a7fb9b64
Will Hanley <willhanley11@gmail.com>
2025-01-03 19:39:27 +0000

    update with individual_player_breakdown

--- main.py
+++ main.py
@@ -6,8 +6,10 @@
 
 from data_collection import get_euroleague_data
 from data_cleaning import clean_playbyplay_data
+from individual_player_breakdown import individual_player_breakdown
 
 def main():
     playbyplay, shotdata, boxdata = get_euroleague_data(2022, 2024)
     cleaned_data = clean_playbyplay_data(playbyplay, boxdata, shotdata)
+    OffensePlayerDataNEW1, DefensePlayerDataNEW1 = individual_player_breakdown(cleaned_data,boxdata)
 
